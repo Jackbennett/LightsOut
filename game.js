473a1d34832cf75110051410ceca5fe37c0a6077
JackBennett <me@jackben.net>
2016-04-04 22:13:49 +0100

    Found horizontal siblings. Now for the vertical siblings...

--- game.js
+++ game.js
@@ -6,9 +6,21 @@ document.addEventListener("DOMContentLoaded", function(){
   board.addEventListener("click", function(event){
     var el = event.target
     if(el.tagName === 'BUTTON'){
-      el.classList.toggle('on')
+      adjacent(el)
     }
   })
   
   
 })
+
+function adjacent(el){
+  if(el.previousElementSibling){
+    el.previousElementSibling.classList.toggle('on')
+  }
+
+  el.classList.toggle('on')
+
+  if(el.nextElementSibling){
+    el.nextElementSibling.classList.toggle('on')
+  }
+}
