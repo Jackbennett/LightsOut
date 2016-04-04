41a5fcb796d7ad6824d90b99c0c18658421aa30d
JackBennett <me@jackben.net>
2016-04-04 22:56:24 +0100

    Ugly but working adjacent function for buttons

--- game.js
+++ game.js
@@ -1,8 +1,16 @@
+window.buttonLength = 0
+
 document.addEventListener("DOMContentLoaded", function(){
   
   
   var board = document.querySelector('section')
   
+  var button = board.querySelectorAll('button')
+  window.buttonLength = board.querySelectorAll('button').length
+  for(var i = 0; i < buttonLength; i += 1){
+    button[i].id = i
+  }
+
   board.addEventListener("click", function(event){
     var el = event.target
     if(el.tagName === 'BUTTON'){
@@ -22,5 +30,13 @@ function adjacent(el){
 
   if(el.nextElementSibling){
     el.nextElementSibling.classList.toggle('on')
+  }
+
+  var id = parseInt(el.id)
+  if(id + 5 < window.buttonLength){
+    document.getElementById(id + 5).classList.toggle('on')
+  }
+  if(id - 5 > 0){
+    document.getElementById(id - 5).classList.toggle('on')
   }
 }
